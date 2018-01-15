7acfdb92122aaae01a2c11c491f6caf21e002672
hh <hh@qq.com>
2018-01-15 10:26:01 +0800

    注释

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 
 // 这个配置文件，起始就是一个 JS 文件，通过 Node 中的模块操作，向外暴露了一个 配置对象
 module.exports = {
-  // 大家已经学会了举一反4， 大家觉得，在配置文件中，需要手动指定 入口 和 出口
+  // 在配置文件中，手动指定 入口 和 出口
   entry: path.join(__dirname, './src/main.js'),// 入口，表示，要使用 webpack 打包哪个文件
   output: { // 输出文件相关的配置
     path: path.join(__dirname, './dist'), // 指定 打包好的文件，输出到哪个目录中去
@@ -30,13 +30,13 @@ module.exports = {
       filename: 'index.html' // 指定生成的页面的名称
     })
   ],
-  module: { // 这个节点，用于配置 所有 第三方模块 加载器 
+  module: { // 用于配置 所有 第三方模块 加载器 
     rules: [ // 所有第三方模块的 匹配规则
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }, //  配置处理 .css 文件的第三方loader 规则
       { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }, //配置处理 .less 文件的第三方 loader 规则
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }, // 配置处理 .scss 文件的 第三方 loader 规则
       { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=7631&name=[hash:8]-[name].[ext]' }, // 处理 图片路径的 loader
-      // limit 给定的值，是图片的大小，单位是 byte， 如果我们引用的 图片，大于或等于给定的 limit值，则不会被转为base64格式的字符串， 如果 图片小于给定的 limit 值，则会被转为 base64的字符串
+      // limit 给定的值，是图片的大小，单位是 byte， 如果引用的 图片，大于或等于给定的 limit值，则不会被转为base64格式的字符串， 如果 图片小于给定的 limit 值，则会被转为 base64的字符串
       { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader 
       { test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/},
       { test: /\.vue$/, use: 'vue-loader' } // 处理 .vue 文件的 loader
@@ -49,8 +49,8 @@ module.exports = {
   }
 }
 
-// 当我们在 控制台，直接输入 webpack 命令执行的时候，webpack 做了以下几步：
-//  1. 首先，webpack 发现，我们并没有通过命令的形式，给它指定入口和出口
+// 当在 控制台，直接输入 webpack 命令执行的时候，webpack 做了以下几步：
+//  1. 首先，webpack 发现，并没有通过命令的形式，给它指定入口和出口
 //  2. webpack 就会去 项目的 根目录中，查找一个叫做 `webpack.config.js` 的配置文件
 //  3. 当找到配置文件后，webpack 会去解析执行这个 配置文件，当解析执行完配置文件后，就得到了 配置文件中，导出的配置对象
 //  4. 当 webpack 拿到 配置对象后，就拿到了 配置对象中，指定的 入口  和 出口，然后进行打包构建；
